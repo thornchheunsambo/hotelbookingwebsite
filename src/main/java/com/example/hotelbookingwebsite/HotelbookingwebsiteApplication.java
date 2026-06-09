0ff737d8dd5aa17b0c34fcc97bd863993890f32d
Thornchheun Sambo <thornchheunsambo@gmail.com>
2026-06-09 09:03:13 +0700

    initial commit

---
+++ src/main/java/com/example/hotelbookingwebsite/HotelbookingwebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.hotelbookingwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelbookingwebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelbookingwebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hotelbookingwebsite/HotelbookingwebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hotelbookingwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelbookingwebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
