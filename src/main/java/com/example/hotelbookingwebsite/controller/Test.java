f103353b2d334524718ad62328cd637610b35d1b
Thornchheun Sambo <thornchheunsambo@gmail.com>
2026-06-11 19:51:05 +0700

    add 2

--- src/main/java/com/example/hotelbookingwebsite/controller/Test.java
+++ src/main/java/com/example/hotelbookingwebsite/controller/Test.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Test {
     @GetMapping("/test")
     public String test(){
-        return "TEST 1";
+        return "TEST 1 Both";
     }
 }
